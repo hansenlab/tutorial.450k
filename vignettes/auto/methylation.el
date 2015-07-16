17a5849c7a3ec6102d965faf2f9f7218476c13d5
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-07-16 16:40:44 -0400

    initial move

---
+++ vignettes/auto/methylation.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "methylation"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "reconstructbioRxiv"
+    "funnorm"
+    "noob"
+    "minfi"
+    "swan"
+    "Touleimat:2012"
+    "bumphunter"
+    "sva1"
+    "sva2"
+    "Jaffe:2014"
+    "Houseman:2012"
+    "Sandoval:2011"
+    "Du:2010"
+    "Bibikova:2011"
+    "Dedeurwaerder:2011")))
+
